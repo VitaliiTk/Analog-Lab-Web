90b9a9585e7fd6d3d59109b69d075aa6fb4543ca
Vitalii <tka4uk.gt41@gmail.com>
2023-12-26 00:17:12 +0600

    first add

---
+++ main.js
@@ -0,0 +1,64 @@
+const btnHome = document.querySelector('#home-page-btn');
+const playContent = document.querySelector('#play-content');
+const homePage = document.querySelector('#home-page');
+const btnClosePage = document.querySelector('#close-page');
+const soundBanksContent = document.querySelector('#sound-banks');
+const homeContent = document.querySelector('#home');
+const exploreContent = document.querySelector('#explore');
+const storeContent = document.querySelector('#store');
+const favoriteBtn = document.querySelector('#favorite');
+const bankName = document.querySelector('.bank-name');
+
+
+function goToHomePage(event) {
+    if (event.currentTarget.textContent == 'overview_key') {
+        playContent.classList.add('display-none')
+        homePage.classList.remove('display-none')
+        event.currentTarget.textContent = 'close';
+        event.currentTarget.style.color = '#0B94C1'
+    } else if (event.currentTarget.textContent == 'close') {
+        event.currentTarget.textContent = 'overview_key';
+        playContent.classList.remove('display-none')
+        homePage.classList.add('display-none')
+        event.currentTarget.style.color = '#9c9c9c'
+    }
+}
+btnHome.addEventListener('mousedown', goToHomePage)
+
+
+// content change in center screen and menu selected item
+function contentChange(element, nameOfPage) {
+    document.querySelectorAll('.content__wrapper').forEach((element) => {
+        element.classList.add('display-none')
+    })
+    nameOfPage.classList.remove('display-none')
+    menuItem.forEach((element) => {
+        element.classList.remove('selected')
+    })
+    element.classList.add('selected')
+}
+const menuItem = document.querySelectorAll('.menu__item');
+menuItem.forEach((element, index) => {
+    element.addEventListener('mousedown', (event) => {
+        switch (index) {
+            case 0:
+                contentChange(element, homeContent)
+                break
+            case 1:
+                contentChange(element, exploreContent)
+                break
+            case 2:
+                contentChange(element, soundBanksContent)
+                break
+            case 3:
+                contentChange(element, storeContent)
+                break
+        }
+    })
+})
+
+
+// favorite btn icon logic
+favoriteBtn.addEventListener('mousedown', (event) => {
+    event.currentTarget.classList.toggle('active')
+})
